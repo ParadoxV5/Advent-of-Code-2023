bac4c33cbbc80615e123bf06edc23cd698a3480d
ParadoxV5 <paradox.ver5@gmail.com>
2023-12-05 23:19:56 -0700

    Day 6
    Still brute-force counting? Who knew Math is useful?

---
+++ 06 Wait For It/main.rb
@@ -0,0 +1,27 @@
+# Let `d` be the distance to beat, `t` be the allowed time,
+# and `s` be the time spent on charging the boat and thus the post-charge speed
+# We have `(t-s)s > d ➡ ts - s² - d > 0`.
+# The quadratic formula will get us the two `s`s where the expression crosses zero.
+# ```
+# a = -1, b = t, c = -d
+# s = (-b ± √(b² - 4ac)) / 2a
+#   = (-t ± √(t² - -4(-d))) / -2
+#   = -t / -2 ± √(t² - 4d) / -2
+#   = t / 2 ± √((t² - 4d) / (-2)²)
+#   = t/2 ± √(t²/4 - d)
+# ```
+calculate =-> (race) do
+  t, d = race.map(&:to_f)
+  # Note: I don’t like using Floats, but here division by 2 is binary-precise
+  mid = t / 2
+  dev = Math.sqrt(t * t / 4 - d)
+  (mid + dev).floor - (mid - dev).ceil + 1 # (min..max).size = max - min + 1
+end
+
+input = File.foreach('input.txt').first(2).map { _1.scan /\d++/ }
+puts(
+  'Part 1',
+  input.transpose.map(&calculate).inject(&:*),
+  'Part 2',
+  calculate.(input.map(&:join))
+)
