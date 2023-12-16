16126903a238a9b5ca7ad9a9311bfd514ee5fdb9
ParadoxV5 <paradox.ver5@gmail.com>
2023-12-16 01:46:43 -0700

    Update Day 16 Part 1 (nearly all comments)

--- 16 The Floor Will Be Lava/main1.rb
+++ 16 The Floor Will Be Lava/main1.rb
@@ -22,7 +22,13 @@ INPUT << ''
 #   * … but now in the (other†) parallel direction and is about to loop itself,
 #     I recon that I have just traversed the untracked end early and remove it from the queue.
 #   † How is it guaranteed? The logic is left as an exercise for the reader.
-#   I implement both cases by not traversing any splitters flagged in {ENERGIZED_BY_Y}.
+# I implement both cases by not traversing any splitters flagged in {ENERGIZED_BY_Y}.
+# 
+# [Update] The idea of using {ENERGIZED_BY_Y} booleans to flag traversal progress turns out not actually
+# stopping repetitions, but it _does_ limit loops to a couple of repeats thanks to implementation details.
+# I analyzed the possibilities and found that prevention is no simple task, so I decided to let what I have lie.
+# After all, Part 2 indeed turns out better done with the opposite approach of intuition
+# __and also can fit a Part 1 reimplementation inside__, so I’ll reinvest there instead.
 
 # Depth-first search, you might call it
 beams = [[0, 0, false, 1]]
@@ -40,7 +46,7 @@ until beams.empty?
         # as if empty space
       in '-' unless dir_is_y
         # ditto
-      in '|' | '-'
+      in '|' | '-' # Perpendicular: split
         dir_is_y = !dir_is_y
         split = [x, y, dir_is_y, -dir_vector]
       in '/'
@@ -48,13 +54,13 @@ until beams.empty?
         dir_vector = -dir_vector
       in '\\'
         dir_is_y = !dir_is_y
-      else # `\n` from off-grid or corrupt input
+      else # Already-traversed splitter, `\n` from off-grid, or corrupt input
         break
       end
       if split
         beams2 << split # {Enumerator} + {StopIteration}? Maybe, but that’s incomprehensible, so I didn’t bother.
       else # If splitting, let another (not necessarily _the_ other (; ) traversal mark the splitter.
-        ENERGIZED.fetch(y)[x] ||= true
+        ENERGIZED.fetch(y)[x] = true
       end
       dir_is_y ? y += dir_vector : x += dir_vector
     end
