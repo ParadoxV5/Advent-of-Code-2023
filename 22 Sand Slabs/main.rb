99b8626f7c2128b97b8483279966d751f526d6c6
ParadoxV5 <paradox.ver5@gmail.com>
2023-12-22 15:37:32 -0700

    Day 22 Part 2
    Refactors Part 1

--- 22 Sand Slabs/main.rb
+++ 22 Sand Slabs/main.rb
@@ -1,16 +1,49 @@
 # https://minecraft.wiki/w/Sandstone_Slab
 
-Slab = Data.define(*%i[x1 y1 z1 x2 y2 z2]) do
+Slab = Struct.new(*%i[x1 y1 z1 x2 y2 z2]) do
+  attr_reader :sits_on
+  def initialize(...)
+    @sits_on = []
+    super
+  end
+  
+  # `other` explicitly hard carries `self` if `self` {#sits_on} `other` and only `other`.
+  # This is that hard carrying slab, or `nil` if `self` `sits_on` other slabs too.
+  # A slab is unsafe to disïntegrate if it hard carries one or more slabs.
+  attr_reader :hard_carried_by
+  def sits_on=(others)
+    @sits_on = others
+    @hard_carried_by = (others.first if others.one?) # It’s not a hard carry if more than one supports it.
+    #@all_hard_carried_by = nil # Purge cache of lazy variable (it’s only accessed once in this puzzle though.)
+  end
+  
+  # [Part 2]
+  # Recursively search all slabs that hard carries `self`, whether {#hard_carried_by explicitly} or implicitly.
+  # The slabs `other` implicitly hard carries are the slabs hard carried
+  # (explicitly or implicitly) by the *collection of* slabs that {#sits_on} `other`.
+  def all_hard_carried_by
+    @all_hard_carried_by ||= if hard_carried_by
+      hard_carried_by.all_hard_carried_by.dup.add hard_carried_by
+    else
+      sits_on.map(&:all_hard_carried_by).inject(&:&) or Set.new
+    end
+  end
+  
+  
   # Check if the two rectangles *don’t* overlap if excluding the Z dimension
   def independent?(other) =
     x1 > other.x2 || x2 < other.x1 || y1 > other.y2 || y2 < other.y1
+  
+  # Note: Modifies `self` unless returning `nil`
   def drop_on?(other)
     unless independent? other
       z1 = other.z2.succ
-      with(z1:, z2: z2 - self.z1 + z1)
+      self.z2 = z2 - self.z1 + z1
+      self.z1 = z1
     end
   end
 end
+
 
 STACK = [Slab.new(0, 0, 0, 9, 9, 0)] # Initialize with a ground
 File.foreach('input.txt')
@@ -18,20 +51,28 @@ File.foreach('input.txt')
   .sort_by!(&:z1) # I don’t think {Slab#z1} vs. {Slab#z2} matters here
   .each do|slab|
     # It’s more a Jenga than a pile, so linear is more efficient than {Array#bsearch}  
-    STACK.reverse_each.find do|other|
-      # Ensure the {STACK} is sorted by {Slab#z2} (if only there is “sorted list”…)
-      if (slab2 = slab.drop_on? other)
-        STACK.insert STACK.rindex { _1.z2 <= slab2.z2 }&.succ || 0, slab2
-      end
-    end
+    STACK.reverse_each.find { slab.drop_on? _1 }
+    # Ensure the {STACK} is sorted by {Slab#z2} (if only there is “sorted list”…)
+    STACK.insert STACK.rindex { _1.z2 <= slab.z2 }&.succ || 0, slab
   end
+# Now that everything has settled, don’t need the ground anymore.
+# (It actually impacts Part 2 – imagine the chain reaction if the Island is disïntegrated 💀.)
+STACK.shift
 
 STACK_BY_Z2 = STACK.group_by(&:z2)
+# Fill in {#sits_on}
+STACK.each do|slab|
+  if (layer_below = STACK_BY_Z2[slab.z1.pred])
+    slab.sits_on =
+      layer_below.reject { slab.independent? _1 or slab.equal? _1 }
+  end
+end
+
+
 puts(
-  STACK.size - STACK.filter_map do|slab|
-    # A slab is unsafe to disintegrate if there are slabs that sit on it and only it.
-    sits_on, not_exactly_one =
-      STACK_BY_Z2[slab.z1.pred]&.reject { slab.independent? _1 or slab.equal? _1 }
-    sits_on unless not_exactly_one
-  end.uniq.size
+  'Part 1',
+  STACK.size - STACK.filter_map(&:hard_carried_by).uniq.size,
+  'Part 2',
+  # Totalling topples per disïntegration is equivalent to totalling disïntegrations per topple.
+  STACK.sum { _1.all_hard_carried_by.size }
 )
